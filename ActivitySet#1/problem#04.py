9252e6f374382ae1b74b95b62befc76729846f5d
GUHARAJ <94677950+GUHARAJ@users.noreply.github.com>
2022-05-26 09:30:46 +0000

    p4.sol

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,19 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+
+score = input("Enter Score: ")
+S = float(score)
+X = 'error'
+if S >= 0.9:
+   X = 'A'
+elif S >= 0.8:
+      X = 'B'
+elif S >= 0.7:
+      X = 'C'
+elif S >= 0.6:
+      X = 'd'
+elif S < 0.6:
+       S = 'F'
+else:
+     X = "out of range"
+print (X)
